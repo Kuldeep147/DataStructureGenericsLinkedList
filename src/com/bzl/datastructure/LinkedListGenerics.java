8fca925c52407222b2c65ba2659d586f0f8858b9
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-25 13:42:00 +0530

    adding uc10

--- src/com/bzl/datastructure/LinkedListGenerics.java
+++ src/com/bzl/datastructure/LinkedListGenerics.java
@@ -3,29 +3,36 @@ package com.bzl.datastructure;
 import java.util.LinkedList;
 
 public class LinkedListGenerics {
+    LinkedList<Integer>  simpleList = new LinkedList<Integer>();
     public static void main(String[] args) {
-        LinkedList<Integer>  simpleList = new LinkedList<Integer>();
-        simpleList.add(56);
-        simpleList.add(30);
-        simpleList.add(70);
-        int i = 0;
-        for(Integer a:simpleList){
-            i++;
-            if(a == 30){
-                break;
-            }
-        }
-        simpleList.add(i,40);
-        int d = 0;
-        for(Integer a:simpleList){
-            d++;
-            if(a == 40){
-                break;
-            }
-        }
-        simpleList.remove(d - 1);
+        LinkedListGenerics ob = new LinkedListGenerics();
+        ob.addSorted(56);
+        ob.addSorted(30);
+        ob.addSorted(40);
+        ob.addSorted(70);
+        ob.addSorted(99);
+        ob.addSorted(1);
+        ob.print();
 
+    }
+    public void print(){
         System.out.println(simpleList);
-
+    }
+    public void addSorted(Integer a){
+        int f = 0;
+        if(simpleList.size() == 0){
+            simpleList.add(a);
+        }else {
+            for(Integer d:simpleList){
+                f++;
+                if (a < d){
+                    simpleList.add(f -1,a);
+                    break;
+                }else if(a > d && f == simpleList.size()){
+                    simpleList.add(a);
+                    break;
+                }
+            }
+        }
     }
 }
