204c8f0e16061fa3ee8ad206dc00dc33f4629930
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-25 12:08:46 +0530

    adding uc1

--- src/com/bzl/datastructure/LinkedListGenerics.java
+++ src/com/bzl/datastructure/LinkedListGenerics.java
@@ -1,6 +1,10 @@
 package com.bzl.datastructure;
+
+import java.util.LinkedList;
+
 public class LinkedListGenerics {
     public static void main(String[] args) {
-       
+        LinkedList<Integer>  simpleList = new LinkedList<Integer>();
+
     }
 }
